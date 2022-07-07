7c0ae38a8ccfa49dda2d2e9b297282b629e6e610
Ivan B <ivar110kg@gmail.com>
2022-07-07 20:37:23 +0400

    Add MySecurityInitializer.java

---
+++ src/main/java/io/github/ivan100kg/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package io.github.ivan100kg.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends
+        AbstractSecurityWebApplicationInitializer {}
